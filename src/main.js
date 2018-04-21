f4d0092ab9ee4097297b8eb1dc820cd339c81ea0
JiangInk <JiangInk@foxmail.com>
2018-04-21 14:40:37 +0800

    按需引入element

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Button, Select } from 'element-ui'
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
 
 Vue.config.productionTip = false
 
