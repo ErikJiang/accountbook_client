e3851bffca54a38f59c6c568c9fa34976da149d5
jiang <jiangink@foxmail.com>
2018-04-26 17:57:24 +0800

    debug

--- src/components/page/AddBill.vue
+++ src/components/page/AddBill.vue
@@ -2,36 +2,113 @@
   <div>
     <v-bread title="新增账目" desc="新增收入支出账目数据" :list="list"></v-bread>
 
-    <h1>{{ msg }}</h1>
-    <h2>AddBill Page</h2>
+    <div class="container">
+      <div class="form-box">
+
+        <el-form :model="createData" ref="createData" label-width="100px" class="demo-ruleForm">
+          
+          <el-form-item label="账单类型">
+            <el-radio-group v-model="createData.billType">
+              <el-radio label="income" border>收入类型</el-radio>
+            <el-radio label="outgo" border>支出类型</el-radio>
+            </el-radio-group>
+          </el-form-item>
+
+          <el-form-item label="分类选择">
+            <el-select v-model="createData.category" placeholder="请选择" clearable>
+              <el-option-group v-for="group in categoryList" :key="group.label" :label="group.label">
+                <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+              </el-option-group>
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="日期设置">
+            <el-date-picker v-model="createData.date" type="date" placeholder="选择日期" value-format='timestamp'></el-date-picker>
+          </el-form-item>
+
+          <el-form-item class='amount' label="金额" prop="amount" :rules="amountRules">
+            <el-input type="amount" v-model.number="createData.amount" auto-complete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('createData')">提交</el-button>
+            <el-button @click="resetForm('createData')">重置</el-button>
+          </el-form-item>
+
+        </el-form>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import vBread from '../common/Bread'
+import vBread from "../common/Bread";
 export default {
-  name: 'AddBill',
-  data () {
+  name: "AddBill",
+  data() {
     return {
-      msg: '新增账目页面',
       list: [
         {
-          name: '新增账目',
-          path: '/addBill'
+          name: "新增账目",
+          path: "/addBill"
         }
-      ]
-    }
+      ],
+      categoryList: [
+        {
+          label: "餐饮",
+          options: [
+            { value: "zaocan", label: "早餐" },
+            { value: "wucan", label: "午餐" },
+            { value: "wancan", label: "晚餐" }
+          ]
+        },
+        {
+          label: "零食烟酒",
+          options: [
+            { value: "shuiguo", label: "水果" },
+            { value: "yinliao", label: "饮料" },
+            { value: "lingshi", label: "零食" },
+            { value: "tangguo", label: "糖果" }
+          ]
+        }
+      ],
+      amountRules: [
+        { required: true, message: "金额不能为空" },
+        { type: "number", message: "金额必须为数字值" }
+      ],
+      createData: {
+        billType: "income",
+        category: "",
+        date: "",
+        amount: 0
+      }
+    };
   },
   components: {
     vBread
+  },
+  methods: {
+    submitForm(data) {
+      console.log(`submitForm data: ${JSON.stringify(this.createData)}`);
+    },
+    resetForm(data) {
+      console.log(`resetForm data: ${JSON.stringify(this.createData)}`);
+      this.createData = {
+        billType: "income",
+        category: "",
+        date: "",
+        amount: 0
+      };
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-  float: right;
+.amount {
+  width: 25%;
 }
 </style>
