2ebcf0012fdc305855b5f2e9d21a652a96c9410b
JiangInk <JiangInk@foxmail.com>
2018-06-13 23:45:39 +0800

    debug

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const url = 'http://127.0.0.1:8000';
+const url = 'http://127.0.0.1:8000'
 
 const instance = axios.create({
   baseURL: url,
@@ -8,31 +8,31 @@ const instance = axios.create({
     'Content-Type': 'application/json'
   },
   timeout: 10000
-});
+})
 
 instance.interceptors.request.use(function (req) {
-  const token = sessionStorage.getItem('token');
+  const token = sessionStorage.getItem('token')
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = token
   }
-  req.headers['Content-Type'] = 'application/json';
-  return req;
+  req.headers['Content-Type'] = 'application/json'
+  return req
 }, function (error) {
-  return Promise.reject(error);
-});
+  return Promise.reject(error)
+})
 
 instance.interceptors.response.use(function (res) {
   if (res.data.code == 20001) {
-    sessionStorage.token = '';
-    $vm.$router.push('/');
+    sessionStorage.token = ''
+    $vm.$router.push('/')
     $vm.$message({
       message: '登录已过期，请重新登录！',
       type: 'warning'
-    });
+    })
   }
-  return res;
+  return res
 }, function (error) {
-  return Promise.reject(error.response);
-});
+  return Promise.reject(error.response)
+})
 
-export default instance;
+export default instance
